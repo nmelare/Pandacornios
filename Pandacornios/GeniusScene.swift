9913219a02c200488075cb8999f978cdbef61fd0
lffattori <luhf.fattori@icloud.com>
2019-10-21 18:17:49 -0300

    Esqueleto do Genius pronto, problemas na função de animar

--- Pandacornios/GeniusScene.swift
+++ Pandacornios/GeniusScene.swift
@@ -11,6 +11,10 @@ import GameplayKit
 import AVKit
 
 class GeniusScene: SKScene {
+
+    var toque = 0
+
+    var playerTurn: Bool = false
     // Atlas
     var redRetangleAtlas: SKTextureAtlas = SKTextureAtlas(named: "redRetangle")
     var yellowRetangleAtlas: SKTextureAtlas = SKTextureAtlas(named: "yellowRetangle")
@@ -44,27 +48,32 @@ class GeniusScene: SKScene {
     func setUpRedRetangelOff() {
         self.addChild(redRetangleOff)
         redRetangleOff.position = CGPoint(x: size.width / 2.5, y: size.height * 0.55)
+        redRetangleOff.name = "RedButton"
     }
 
     func setUpYellowRetangelOff() {
         self.addChild(yellowRetangleOff)
         yellowRetangleOff.position = CGPoint(x: size.width / 2.5, y: size.height * 0.45)
+        yellowRetangleOff.name = "YellowButton"
     }
 
     func setUpGreenRetangelOff() {
         self.addChild(greenRetangleOff)
         greenRetangleOff.position = CGPoint(x: size.width / 1.65, y: size.height * 0.55)
+        greenRetangleOff.name = "GreenButton"
     }
 
     func setUpBlueRetangelOff() {
         self.addChild(blueRetangleOff)
         blueRetangleOff.position = CGPoint(x: size.width / 1.65, y: size.height * 0.45)
+        blueRetangleOff.name = "BlueButton"
     }
 
     func setUpPlayButton() {
         self.addChild(buttonPlay)
         buttonPlay.position = CGPoint(x: size.width / 2, y: size.height / 1.5)
         buttonPlay.size = CGSize(width: size.width / 4, height: size.height / 4)
+        buttonPlay.name = "PlayButton"
     }
 
     override var isUserInteractionEnabled: Bool {
@@ -77,24 +86,102 @@ class GeniusScene: SKScene {
     }
 
     func playGame() {
-        gameSequel = []
-        var elementOfTheSequel = buttonsOptions.randomElement()
+        let elementOfTheSequel = buttonsOptions.randomElement()
         gameSequel.append(elementOfTheSequel!)
 
     }
 
-    func playSequel() {
-        SKAction.animate(with: <#T##[SKTexture]#>, timePerFrame: <#T##TimeInterval#>)
+    func animate( animatedRetangles: [SKSpriteNode]) {
+
+        for rect in animatedRetangles {
+            var textureOfRetangles:[SKTexture] = []
+            if let retangleTexture = rect.texture {
+//                paramos aqui.
+                func loadFrames(_ textureAtlas: SKTextureAtlas) -> [SKTexture] {
+
+                    var frames = [SKTexture]()
+                    let names = textureAtlas.textureNames.sorted()
+
+                    for i in 0...names.count-1 {
+                        let t = textureAtlas.textureNamed(names[i])
+                        t.filteringMode = .nearest
+                        frames.append(t)
+                    }
+                    return frames
+                }//AQUI
+                textureOfRetangles.append(retangleTexture)
+            }
+
+            let action = SKAction.animate(withNormalTextures: textureOfRetangles, timePerFrame: 0.5, resize: false, restore: true)
+            rect.run(action) {
+                self.playerTurn = true
+                self.resetPlayerGameSequel()
+                print("Go player")
+            }
+        }
+
     }
 
+    func animateSequel() {
+        self.animate(animatedRetangles: gameSequel)
+    }
+
+    func resetGame() {
+        gameSequel = []
+        playerSequel = []
+    }
+
+    func resetPlayerGameSequel() {
+        playerSequel = []
+    }
 // Se o toque começou
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first
             else {
                 return
         }
+
         let location = touch.location(in: self)
 
-    }
+        guard let frontTouchedNode = self.atPoint(location) as? SKSpriteNode else { return }
 
+        if frontTouchedNode.name == "PlayButton" {
+            self.resetGame()
+            self.playGame()
+            self.animateSequel()
+            print("Passou do start")
+        } else {
+            return
+        }
+        if playerTurn == true {
+            if buttonsOptions.contains(frontTouchedNode) {
+                if frontTouchedNode == redRetangleOff {
+                    animate(animatedRetangles: [redRetangleOff])
+                }
+                if frontTouchedNode == blueRetangleOff {
+                    animate(animatedRetangles: [blueRetangleOff])
+                }
+                if frontTouchedNode == yellowRetangleOff {
+                    animate(animatedRetangles: [yellowRetangleOff])
+                }
+                if frontTouchedNode == greenRetangleOff {
+                    animate(animatedRetangles: [greenRetangleOff])
+                }
+                playerSequel.append(frontTouchedNode)
+
+                if playerSequel[toque] == gameSequel[toque] {
+                    toque += 1
+                } else {
+//                    som de perdeu
+                    resetPlayerGameSequel()
+                    resetGame()
+                }
+
+            }
+        } else {
+            isUserInteractionEnabled = false
+            self.animateSequel()
+        }
+
+    }
 }
