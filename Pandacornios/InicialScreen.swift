ee6ba419475b927d31109e08bc97a3aceb5bcd0d
Nathalia <nathaliamelare@gmail.com>
2019-10-24 15:32:26 -0300

    Transicao feita com bugs

--- Pandacornios/InicialScreen.swift
+++ Pandacornios/InicialScreen.swift
@@ -12,34 +12,49 @@ import GameplayKit
 class InicialScreen: SKScene {
     
     var background: SKSpriteNode = SKSpriteNode(imageNamed: "Background")
+    var hiddenNodeGenius: SKSpriteNode = SKSpriteNode(imageNamed: "redRetangleOff")
+    var hiddenNodeSnake: SKSpriteNode = SKSpriteNode(imageNamed: "redRetangleOff")
+    var hiddenNodeSpaceInvaders: SKSpriteNode = SKSpriteNode(imageNamed: "redRetangleOff")
     
     override func sceneDidLoad() {
-        self.physicsVase()
-
+        self.backgroundSetUp()
+        self.hiddeNodeGeniusSetUp()
+        self.hiddeNodeSpaceInvadersSetUp()
+        self.hiddeNodeSnakeSetUp()
     }
-//    override func didMove(to view: SKView) {
-//        self.physicsVase()
-//        backgroundColor =  #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
-//        }
-        
-    func physicsVase() {
+    
+    func backgroundSetUp() {
         self.addChild(background)
-        print(background.position)
         background.name = "Background"
         background.size = CGSize (width: size.height * 1, height: size.width * 1)
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         background.zRotation = (.pi / 2)
-        background.isUserInteractionEnabled = false
     }
     
-    override var isUserInteractionEnabled: Bool {
-        get {
-            return true
-        }
-        set {
-            // ignore
-        }
-    }
+    func hiddeNodeGeniusSetUp() {
+           self.addChild(hiddenNodeGenius)
+        hiddenNodeGenius.size = CGSize (width: size.height * 0.05, height: size.width * 0.1)
+        hiddenNodeGenius.position = CGPoint(x: size.width * 0.8 , y: size.height * 0.3)
+        hiddenNodeGenius.zPosition = +1
+//        hiddenNodeGenius.isHidden = true
+    
+       }
+    
+    func hiddeNodeSnakeSetUp() {
+              self.addChild(hiddenNodeSnake)
+           hiddenNodeSnake.size = CGSize (width: size.height * 0.06, height: size.width * 0.15)
+           hiddenNodeSnake.position = CGPoint(x: size.width * 0.43 , y: size.height * 0.47)
+//           hiddenNodeSnake.isHidden = true
+       
+          }
+    
+    func hiddeNodeSpaceInvadersSetUp() {
+              self.addChild(hiddenNodeSpaceInvaders)
+           hiddenNodeSpaceInvaders.size = CGSize (width: size.height * 0.06, height: size.width * 0.2)
+        hiddenNodeSpaceInvaders.position = CGPoint(x: size.width * 0.35 , y: size.height * 0.3)
+//           hiddenNodeSpaceInvaders.isHidden = true
+       
+          }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {
@@ -47,15 +62,26 @@ class InicialScreen: SKScene {
         }
         
         let location = touch.location(in: self)
-        print(location)
         
-        let touchedNodes = nodes(at: location)
-
         let frontTouchedNode = self.atPoint(location)
 
-        if (frontTouchedNode.name == "vaso") {
+        if (frontTouchedNode.contains(hiddenNodeGenius.position)) {
             let newScene = GeniusScene(size: self.size)
                            
+            let doorsClose = SKTransition.doorsCloseVertical(withDuration: 1.0)
+            view?.presentScene(newScene, transition: doorsClose)
+        }
+        
+        if (frontTouchedNode.contains(hiddenNodeSnake.position)) {
+            let newScene = GameSnake(size: self.size)
+
+            let doorsClose = SKTransition.doorsCloseVertical(withDuration: 1.0)
+            view?.presentScene(newScene, transition: doorsClose)
+        }
+
+        if (frontTouchedNode.contains(hiddenNodeSpaceInvaders.position)) {
+            let newScene = SpaceInvaders(size: self.size)
+
             let doorsClose = SKTransition.doorsCloseVertical(withDuration: 1.0)
             view?.presentScene(newScene, transition: doorsClose)
         }
