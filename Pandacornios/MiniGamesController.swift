0c4da603de03e4f5f4fa17133ea1ce482d16179d
lffattori <luhf.fattori@icloud.com>
2019-10-29 14:19:44 -0300

    Teste de retirada do genius da tela principal

---
+++ Pandacornios/MiniGamesController.swift
@@ -0,0 +1,9 @@
+//
+//  MiniGamesController.swift
+//  Pandacornios
+//
+//  Created by Luiza Fattori on 29/10/19.
+//  Copyright © 2019 Nathalia Melare. All rights reserved.
+//
+
+import Foundation
